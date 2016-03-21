25588558619b331a299caf990461d0fbfca25a23
morgondag <kim@morgondag.nu>
2016-03-21 22:04:16 +0100

    simplified renderer

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const render = require('./render.js')
-console.log(render('app.jsx'))
+const React = require('react')
+const ReactDOMServer = require('react-dom/server')
+require('babel-core/register')
+
+module.exports = (file, options, isStatic) => {
+  const reactElement = React.createElement(require(file).component, options)
+  try {
+    if (isStatic) {
+      return ReactDOMServer.renderToStaticMarkup(reactElement)
+    }
+    return ReactDOMServer.renderToString(reactElement)
+  } catch(e) {
+    return e
+  }
+}
